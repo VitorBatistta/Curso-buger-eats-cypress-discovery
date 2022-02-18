9077343dad73a99c4a1551dd4474e341dd3705b6
vitor.batista.svm <vitor.batista.svm@gmail.com>
2022-02-18 13:46:51 -0300

    Primeira versão

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,77 @@
+import signupPage from '../pages/SignupPage'
+import signupFactory from '../factories/SignupFactory'
+
+
+
+describe('Signup',() => {
+
+   /* beforeEach( function (){
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d;
+        })
+    })*/
+    it.skip('User dhould be deliver', function () {
+        var deliver = signupFactory.deliver()
+
+        signupPage.go()
+        signupPage.fillForm(deliver)
+        signupPage.submit()
+
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
+        signupPage.modalContentShouldBe(expectedMessage)
+    })
+
+    it.skip('Incorrect document', function () {
+        var deliver = signupFactory.deliver()
+        
+        deliver.cpf = '000022244aa'
+        signupPage.go()
+        signupPage.fillForm(deliver)
+        signupPage.submit()
+        signupPage.alertMessageShouldBe('Oops! CPF inválido')
+    })
+
+    it.skip('Incorrect email', function () {
+        var deliver = signupFactory.deliver()
+        deliver.email = 'email.com.br'
+        signupPage.go()
+        signupPage.fillForm(deliver)
+        signupPage.submit()
+        signupPage.alertMessageShouldBe('Oops! Email com formato inválido.')
+    })
+
+    context('Require Fields', function(){
+        const messages = [
+            {field: 'name', output: 'É necessário informar o nome' },
+            {field: 'cpf', output: 'É necessário informar o CPF' },
+            {field: 'email', output: 'É necessário informar o email' },
+            {field: 'postalcode', output: 'É necessário informar o CEP' },
+            {field: 'number', output: 'É necessário informar o número do endereço' },
+            {field: 'dilivery_method', output: 'Selecione o método de entrega' },
+            {field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+        ]
+        before(function (){
+            signupPage.go()
+            signupPage.submit()
+        })
+
+        messages.forEach(function(msg){
+            it(`${msg.field} is required`, function(){
+                signupPage.alertMessageShouldBe(msg.output)
+            })
+        })
+    })
+
+    it('Required fields', function(){
+        signupPage.go()
+        signupPage.submit()
+        signupPage.alertMessageShouldBe('É necessário informar o nome')
+        signupPage.alertMessageShouldBe('É necessário informar o CPF')
+        signupPage.alertMessageShouldBe('É necessário informar o email')
+        signupPage.alertMessageShouldBe('É necessário informar o CEP')
+        signupPage.alertMessageShouldBe('É necessário informar o número do endereço')
+        signupPage.alertMessageShouldBe('Selecione o método de entrega')
+        signupPage.alertMessageShouldBe('Adicione uma foto da sua CNH')
+
+    })
+}) 
